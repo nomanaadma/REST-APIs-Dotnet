849616b494701366bcd23bd5ab725d3a538c5b6c
Noman Shoukat <nomanaadma@gmail.com>
2024-08-07 20:16:30 +0500

    initializing db

--- Movies.Api/Program.cs
+++ Movies.Api/Program.cs
@@ -1,8 +1,11 @@
 using Movies.Application;
+using Movies.Application.Database;
 
 var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
 
 builder.Services.AddControllers();
+
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -10,6 +13,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddApplication();
+builder.Services.AddDatabase(config["Database:ConnectionString"]);
+
 
 var app = builder.Build();
 
@@ -23,5 +28,9 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.MapControllers();
+
+var dbInitializer = app.Services.GetRequiredService<DbInitializer>();
+await dbInitializer.InitializeAsync();
+
 
 app.Run();
