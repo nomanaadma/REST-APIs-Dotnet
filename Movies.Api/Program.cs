d493debf52ac7de4b3e3bb6697d48a6d7e9e2534
Noman Shoukat <nomanaadma@gmail.com>
2024-08-07 23:29:04 +0500

    validation and token

--- Movies.Api/Program.cs
+++ Movies.Api/Program.cs
@@ -1,3 +1,4 @@
+using Movies.Api.Mapping;
 using Movies.Application;
 using Movies.Application.Database;
 
@@ -27,6 +28,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseMiddleware<ValidationMappingMiddleware>(); 
+    
 app.MapControllers();
 
 var dbInitializer = app.Services.GetRequiredService<DbInitializer>();
