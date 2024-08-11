a109ae6110dc072205f73fa205d2c478f8413de1
Noman Shoukat <nomanaadma@gmail.com>
2024-08-11 06:35:24 +0500

    refit http client integration

--- Movies.Api.Sdk.Consumer/Program.cs
+++ Movies.Api.Sdk.Consumer/Program.cs
@@ -1,9 +1,21 @@
 ﻿using System.Text.Json;
+using Microsoft.Extensions.DependencyInjection;
 using Movies.Api.Sdk;
 using Movies.Contracts.Requests;
 using Refit;
 
-var moviesApi = RestService.For<IMoviesApi>("https://localhost:5001");
+// var moviesApi = RestService.For<IMoviesApi>("https://localhost:5001");
+
+var serivces = new ServiceCollection();
+
+serivces.AddRefitClient<IMoviesApi>()
+	.ConfigureHttpClient(x =>
+		x.BaseAddress = new Uri("https://localhost:5001"));
+
+var provider = serivces.BuildServiceProvider();
+
+var moviesApi = provider.GetRequiredService<IMoviesApi>();
+
 
 // var movie = await moviesApi.GetMovieAsync("toy-story-1995");
 
